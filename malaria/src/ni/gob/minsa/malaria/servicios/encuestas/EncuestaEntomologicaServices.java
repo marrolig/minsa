5c5617c703885ff06da0f7f4b46536c538894141
mdeltrus <mauriciojdelgadot@gmail.com>
2013-10-13 20:00:20 -0600

    prueba

--- malaria/src/ni/gob/minsa/malaria/servicios/encuestas/EncuestaEntomologicaServices.java
+++ malaria/src/ni/gob/minsa/malaria/servicios/encuestas/EncuestaEntomologicaServices.java
@@ -1,5 +1,9 @@
 package ni.gob.minsa.malaria.servicios.encuestas;
 
+import ni.gob.minsa.ciportal.dto.InfoResultado;
+
 public interface EncuestaEntomologicaServices {
 
+	public InfoResultado guardarEncuesta();
+	
 }
