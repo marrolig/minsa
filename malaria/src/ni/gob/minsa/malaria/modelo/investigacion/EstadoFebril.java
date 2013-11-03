a266188665f36cd19974eefcb29cbf1ccedb34d4
Felix Medina <medina.fx@gmail.com>
2013-11-02 23:16:03 -0600

    Change-Id: I11140638e23a8ad3a07c7181757fc1885f18b920

---
+++ malaria/src/ni/gob/minsa/malaria/modelo/investigacion/EstadoFebril.java
@@ -0,0 +1,30 @@
+package ni.gob.minsa.malaria.modelo.investigacion;
+
+import java.io.Serializable;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+
+import org.eclipse.persistence.annotations.Cache;
+
+import ni.gob.minsa.malaria.modelo.general.Catalogo;
+
+
+/**
+ * Clase de persistencia para la tabla CATALOGOS
+ * con el criterio de filtro para clasificación 
+ * clínica bajo el código ESTF
+ */
+@Entity
+@Inheritance(strategy=InheritanceType.SINGLE_TABLE)
+@DiscriminatorValue(value="ESTF")
+@Cache(alwaysRefresh=true,disableHits=true)
+public class EstadoFebril extends Catalogo implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	public EstadoFebril(){
+		
+	}
+}
